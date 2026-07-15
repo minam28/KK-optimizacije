2cc5616d8ce48712abaa896388d7bbddae022aaf
minam28 <73477911+minam28@users.noreply.github.com>
2026-07-15 14:14:25 +0200

    Create test4.c

---
+++ DCE/Test primeri/test4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fja(int uslov, int x, int y){
+  int a=0, b=0;
+  if (uslov) {
+    a = x + 10;
+    y = a;
+  } 
+  else {
+    a = x + 10;
+    y = a;
+}
+  return y;
+}
