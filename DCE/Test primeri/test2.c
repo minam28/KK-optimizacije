5a37944ea9fa61be677b76146b2f24cb092e316b
minam28 <73477911+minam28@users.noreply.github.com>
2026-07-15 14:03:54 +0200

    Create test2.c

---
+++ DCE/Test primeri/test2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int test_func(int a, int b) {
+    int result;
+    if (a > b) {
+        result = (a + b) * 2;
+    } else {
+        result = (a + b) * 2;
+    }
+    return result;
+}
